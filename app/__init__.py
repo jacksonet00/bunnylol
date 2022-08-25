1a0ffcfba0039c5522b8de284e25843a38ec07b0
Jackson Taylor <jacksoneliastaylor@gmail.com>
2022-08-24 22:59:28 -0400

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from os import environ
 from flask import Flask, request, redirect
-from constants import alias_table
+
+from constants import alias_table # Dict[str: str] alias => url
 
 def create_app():
     app = Flask(__name__)
@@ -10,12 +11,27 @@ def create_app():
     def bunnylol():
         query = request.args.get('q')
 
+        # alias redirect
         if query in alias_table:
             return redirect(alias_table[query])
         
-        if query[:2] == 'gh':
-            return redirect(f'https://github.com/search?q={query[2:]}')
+        # github search
+        if query[:3] == 'gh ':
+            return redirect(f'https://github.com/search?q={query[3:]}')
+        
+        # wikipedia search
+        if query[:2] == 'w ':
+            return redirect(f'https://en.wikipedia.org/w/index.php?search={query[2:]}')
+        
+        # stack overflow search
+        if query[:3] == 'so ':
+            return redirect(f'https://stackoverflow.com/search?q={query[3:]}')
+        
+        # amazon search
+        if query[:2] == 'a ':
+            return redirect(f'https://www.amazon.com/s?k={query[2:]}')
 
+        # fallback to google search by default
         return redirect(f'https://google.com/search?q={query}')
 
     return app
